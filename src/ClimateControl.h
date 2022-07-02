954c2a43404eea5af44babdf41ee969c2332e363
Tim <15017472+doodlum@users.noreply.github.com>
2022-07-02 17:25:08 +0100

    Patch sun update to correct direction

--- src/ClimateControl.h
+++ src/ClimateControl.h
@@ -13,9 +13,16 @@ public:
 protected:
 	struct Patches
 	{
+		static double __fastcall SunUpdate_GetYExtremeHook(void* _this, void* _edx)
+		{
+			double result = ((double(__thiscall*)(void*))0x00450410)(_this);
+			return -result;
+		}
+
 		static void Install()
 		{
 			WriteRelJump(0x63AD5F, 0x63AD9D);  // Patch interior check so weathers always update
+			WriteRelCall(0x00641830 + 0xA68, (UInt32)&SunUpdate_GetYExtremeHook); // Patch sun update to correct direction
 		}
 	};
 };
